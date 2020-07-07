80b0af9dd608ad6be2dba73f9f0493ccbb61c5a5
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-07 18:01:13 +0100

    fixed cors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [movies, setMovie] = useState(['Movie 1', 'Movie 2'])
+  const [movies, setMovie] = useState([])
+
+  useEffect(()=> {
+    fetch(' http://127.0.0.1:8000/api/movies/', {
+      method:'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token a53382b391321ba72afc15cee5ab712239382438'
+      }
+    }).then(response => response.json())
+    .then(data => setMovie(data))
+    .catch(error => console.log(error))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +23,7 @@ function App() {
        <div className="layout">
          <div>
            { movies.map(movie => {
-             return <h2>{movie}</h2>
+             return <h2>{movie.title}</h2>
            })}
          </div>
          <div>Movie details</div>
