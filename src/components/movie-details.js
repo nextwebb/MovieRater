46426b4fc70deaafa94d6ee6b3c2fe68f081bef8
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-08 04:09:19 +0100

    rating stars

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function MovieDetails(props){
+
+    const [ higlighted, setHiglighted] = useState(-1)
+
     const movie = props.movie;
     return (
         <div>
             {
                movie ? (
-                    <div>
+                    <React.Fragment>
                         <h1>{movie.title}</h1>
                         <p>{movie.description}</p>
                         <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 0? 'orange' : ''}/>
@@ -17,7 +20,17 @@ function MovieDetails(props){
                         <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 3? 'orange' : ''} />
                         <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 4? 'orange' : ''}/> 
                         ({movie.no_of_ratings})
-                    </div>
+                        <div className="rate-container">
+                            <h2>Rate it</h2>
+                            {[...Array(5)].map((elem, index ) => {
+                                return  <FontAwesomeIcon key={index} icon={faStar} className={higlighted > index - 1? 'purple' : ''}
+                                    onMouseEnter={setHiglighted(index)}
+                                    onMouseLeave={setHiglighted(-1)}
+                                />
+                            } )}
+                        </div>
+                    </React.Fragment>
+                   
                 ): null
             }
         </div>
