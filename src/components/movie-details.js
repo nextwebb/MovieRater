880acff30e057f32e01192e944c5f9114305d1be
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-08 03:35:49 +0100

    font icon

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function MovieDetails(props){
 
@@ -9,6 +11,7 @@ function MovieDetails(props){
                     <div>
                         <h1>{props.movie.title}</h1>
                         <p>{props.movie.description}</p>
+                        <FontAwesomeIcon icon={faStar} />
                     </div>
                 ): null
             }
