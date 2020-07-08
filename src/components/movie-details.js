2382ae49b615c63d8c46d4216bbd47f6d732b95f
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-08 03:44:22 +0100

    display ratings

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -3,15 +3,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function MovieDetails(props){
-
+    const movie = props.movie;
     return (
         <div>
             {
-                props.movie ? (
+               movie ? (
                     <div>
-                        <h1>{props.movie.title}</h1>
-                        <p>{props.movie.description}</p>
-                        <FontAwesomeIcon icon={faStar} />
+                        <h1>{movie.title}</h1>
+                        <p>{movie.description}</p>
+                        <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 0? 'orange' : ''}/>
+                        <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 1? 'orange' : ''} />
+                        <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 2? 'orange' : ''} />
+                        <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 3? 'orange' : ''} />
+                        <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 4? 'orange' : ''}/> 
+                        ({movie.no_of_ratings})
                     </div>
                 ): null
             }
